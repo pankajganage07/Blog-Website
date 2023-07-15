02de8df47ad07d05c8a834d78d6a9503a71860fc
pankajs2272003 <pankajganage22@gmail.com>
2023-07-15 11:20:51 +0530

    add a local database to blog site

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const path = require("path");
 var _ = require('lodash');
+const mongoose = require('mongoose');
+const mongodb = require('mongodb');
+
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-let posts = []
 
 const app = express();
 
@@ -19,8 +22,30 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+main().catch(err => console.log(err));
 
-app.get('/', function (req, res) {
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/BlogDB');
+
+const entrySchema = new mongoose.Schema({
+  title: String,
+  content: String
+});
+
+//create model for the same schema
+const Entry = mongoose.model('Entry', entrySchema);
+
+
+
+app.get('/', async function (req, res) {
+  //get all the entries/post here and pass it into the post array
+  let posts = [];
+  try{
+    posts = await Entry.find();
+  }catch(err){
+    console.log(err);
+  }
+
   //res.render is used in ejs to render ejs files, ejs files should be defined in 
   //views folder and just specify file name while rendering file
   res.render('home',{homeStartingContent: homeStartingContent, posts: posts});
@@ -38,29 +63,33 @@ app.get('/compose', function (req, res) {
   res.render('compose');
 });
 
-app.get('/posts/:rt', function(req, res){
+app.get('/posts/:rt', async function(req, res){
   let temp = _.lowerCase(req.params.rt);
-
-  posts.find(find_match)
-
-
-  function find_match(item){
-    if(_.lowerCase(item.title) ===  temp){
-      console.log("match found");
-      res.render('post', {
-        title: item.title,
-        content: item.content
-      });
-    }
+  try{
+    const document = await Entry.findOne({title:{'$regex' : temp, '$options' : 'i'}});
+      if(document != null){
+        console.log('match found');
+        res.render('post', {
+          title: document.title,
+          content: document.content
+        });
+      }else{
+        console.log("match not found");
+        res.redirect('/');
+      }
+  }catch(err){
+    console.log(err);
   }
+
 });
 
 app.post('/compose', function(req, res){
-  var data = {
+  const e1 = new Entry({
     title: req.body.compose_title,
     content: req.body.compose_post
-  }
-  posts.push(data);
+  });
+  //insert the newely created entry in database
+  e1.save();
   res.redirect('/');
 })
 
@@ -72,3 +101,4 @@ app.post('/compose', function(req, res){
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
+}
